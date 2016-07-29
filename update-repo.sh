b34fe14dbf9bde1450b3e57a1ee4afb31e0c7b2f
aheadley <aheadley@waysaboutstuff.com>
2016-07-29 16:05:02 -0400

    fix some url errors

--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 for repo_dir in rpm/{i686,x86_64,noarch,source}/; do
-    createrepo -v --deltas $repo_dir
+    createrepo -v --update \
+        "$repo_dir"
 done
 
-aws s3 sync --exclude .git --exclude update-repo.sh ./ s3://aheadley-repo/
+aws s3 sync \
+    --exclude '.git*' \
+    --exclude '.git/*' \
+    --exclude 'update-repo.sh' \
+    ./ s3://aheadley-repo/
